6ef39cc51f5212208e2c23978aae92fe8b096345
Beam <ondra@bimovi.cz>
2014-04-21 23:00:15 +0200

    serazeni addonu pri jejich releasu

--- tools/addons_xml_generator.py
+++ tools/addons_xml_generator.py
@@ -49,7 +49,7 @@ class Generator:
  
     def _generate_addons_file( self ):
         # addon list
-        addons = os.listdir( "source" )
+        addons = sorted(os.listdir( "source" ))
         # final addons text
         addons_xml = u("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<addons>\n")
         # loop thru and add each addons addon.xml file
